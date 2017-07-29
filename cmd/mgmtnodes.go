c61a0e6c0d15f27a03eb519dceaeac95c13370aa
Jakub Scholz <www@scholzj.com>
2017-07-29 23:21:01 +0200

    Add GET_MGMT_NODES operation

--- cmd/mgmtnodes.go
+++ cmd/mgmtnodes.go
@@ -17,33 +17,49 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/scholzj/amqpctl/mgmtlink"
+	"os"
+	"bytes"
+	"github.com/scholzj/amqpctl/operation"
 )
 
 // mgmtnodesCmd represents the mgmtnodes command
 var mgmtnodesCmd = &cobra.Command{
 	Use:   "mgmtnodes",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Retrieve the list of addresses of other Management Nodes",
+	Long: `Retrieve the list of addresses of other Management Nodes which this Management Node is aware of.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("mgmtnodes called")
+		getMgmtNodes(args)
 	},
 }
 
 func init() {
 	getCmd.AddCommand(mgmtnodesCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func getMgmtNodes(args []string) {
+	link := mgmtlink.AmqpMgmtLink{}
+	err := link.ConfigureConnection(amqpCfg)
+	if err != nil {
+		fmt.Printf("Failed to configure AMQP connection: %v\n", err.Error())
+		os.Exit(1)
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// mgmtnodesCmd.PersistentFlags().String("foo", "", "A help for foo")
+	err = link.Connect()
+	if err != nil {
+		fmt.Printf("Failed to connect to AMQP endpoint: %v\n", err.Error())
+		os.Exit(1)
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// mgmtnodesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	defer link.Close()
+
+	var output bytes.Buffer
+	output, err = operation.GetMgmtNodes(&link, "")
+
+	if err == nil {
+		fmt.Print(output.String())
+	} else {
+		fmt.Printf("AMQP Management operation failed: %v\n", err.Error())
+		os.Exit(1)
+	}
 }
