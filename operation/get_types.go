ced9af5010d5d2950e4e330f3193aab5dc3ee87f
Jakub Scholz <www@scholzj.com>
2017-07-29 22:56:19 +0200

    Some minor improvements to repsosne parsing - the interation over individual rows is not needed, just apped the row to the slice and Go will handle the rest

--- operation/get_types.go
+++ operation/get_types.go
@@ -12,42 +12,14 @@ func GetTypes(link mgmtlink.MgmtLink, entityType string) (output bytes.Buffer, e
 	body, err := Get(link, "GET-TYPES", entityType)
 
 	if err == nil {
-		rows := parseResults(body)
+		rows := parseGetTypesResults(body)
 		output = formatter.FormatPlainText([]string{"TYPE", "PARENT"}, rows)
 	}
 
 	return
 }
 
-/*func GetTypes(link mgmtlink.MgmtLink, entityType string) (output bytes.Buffer, err error) {
-	var reqProperties map[string]interface{}
-
-	if entityType != "" {
-		reqProperties = map[string]interface{}{"operation": "GET-TYPES", "entityType": entityType}
-	} else {
-		reqProperties = map[string]interface{}{"operation": "GET-TYPES"}
-	}
-
-	respProperties, respBody, err := link.Operation(reqProperties, nil)
-
-	if err == nil {
-		if respProperties["statusCode"].(int64) == 200 {
-			rows := parseResults(respBody)
-			output = formatter.FormatPlainText([]string{"TYPE", "PARENT"}, rows)
-		} else {
-			err = errors.New(fmt.Sprintf("AMQP Management operation wasn't successfull: %v (%v)\n", respProperties["statusCode"], respProperties["statusDescription"]))
-		}
-	} else {
-		err = errors.New(fmt.Sprintf("AMQP Management operation failed: %v\n", err.Error()))
-	}
-
-	return
-}*/
-
-func parseResults(body interface{}) (rows [][]string) {
-	rows = make([][]string, len(map[interface{}]interface{}(body.(amqp.Map))))
-
-	i := 0
+func parseGetTypesResults(body interface{}) (rows [][]string) {
 	for entitytype, extends := range map[interface{}]interface{}(body.(amqp.Map)) {
 		parents := make([]string, len([]interface{}(extends.(amqp.List))))
 		for j, parent := range []interface{}(extends.(amqp.List)) {
@@ -57,8 +29,7 @@ func parseResults(body interface{}) (rows [][]string) {
 		row := make([]string, 2)
 		row[0] = entitytype.(string)
 		row[1] = strings.Join(parents, ", ")
-		rows[i] = row
-		i++
+		rows = append(rows, row)
 	}
 
 	return
