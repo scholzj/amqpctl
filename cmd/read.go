2371093f96ca7592711072156a889895bc5ee084
Jakub Scholz <www@scholzj.com>
2017-08-01 22:06:32 +0200

    Add READ operation

---
+++ cmd/read.go
@@ -0,0 +1,75 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/scholzj/amqpctl/mgmtlink"
+	"os"
+	"bytes"
+	read_operation "github.com/scholzj/amqpctl/operation/read"
+)
+
+var attributeName string
+
+// readCmd represents the read command
+var readCmd = &cobra.Command{
+	Use:   "read [identity/key]",
+	Short: "Retrieve the attributes of a Manageable Entity.",
+	Long: `Retrieve the attributes of a Manageable Entity.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		read(args)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(readCmd)
+	readCmd.Flags().StringVar(&attributeName,"attribute","identity", "Read based on specific attribute (index)")
+}
+
+func read(args []string) {
+	link := mgmtlink.AmqpMgmtLink{}
+	err := link.ConfigureConnection(amqpCfg)
+	if err != nil {
+		fmt.Printf("Failed to configure AMQP connection: %v\n", err.Error())
+		os.Exit(1)
+	}
+
+	err = link.Connect()
+	if err != nil {
+		fmt.Printf("Failed to connect to AMQP endpoint: %v\n", err.Error())
+		os.Exit(1)
+	}
+
+	defer link.Close()
+
+	var identityOrKey string
+	if len(args) > 0 {
+		identityOrKey = args[0]
+	} else {
+		identityOrKey = ""
+	}
+
+	var output bytes.Buffer
+	output, err = read_operation.Read(&link, identityOrKey, attributeName)
+
+	if err == nil {
+		fmt.Print(output.String())
+	} else {
+		fmt.Printf("AMQP Management operation failed: %v\n", err.Error())
+		os.Exit(1)
+	}
+}
---
+++ operation/read/read.go
@@ -0,0 +1,59 @@
+package read
+
+import (
+	"qpid.apache.org/amqp"
+	"github.com/scholzj/amqpctl/mgmtlink"
+	"github.com/scholzj/amqpctl/formatter"
+	"bytes"
+	"fmt"
+	"errors"
+)
+
+func Read(link mgmtlink.MgmtLink, identity string, attributeName string) (output bytes.Buffer, err error) {
+	var reqProperties map[string]interface{}
+
+	if attributeName == "identity" {
+		reqProperties = map[string]interface{}{"operation": "READ", "identity": identity}
+	} else {
+		reqProperties = map[string]interface{}{"operation": "READ", "index": attributeName, "key": identity}
+	}
+
+	respProperties, respBody, err := link.Operation(reqProperties, nil)
+
+	var statusCode int
+	switch respProperties["statusCode"].(type) {
+	case int32:
+		statusCode = int(respProperties["statusCode"].(int32))
+	case int64:
+		statusCode = int(respProperties["statusCode"].(int64))
+	}
+
+	if err == nil {
+		if statusCode == 200 {
+			headers := []string{"ATTRIBUTE", "VALUE"}
+			rows := parseReadResults(respBody)
+			output = formatter.FormatPlainText(headers, rows)
+		} else if statusCode == 400 {
+			err = errors.New(fmt.Sprintf("Specified index is not supported: %v (%v)\n", respProperties["statusCode"], respProperties["statusDescription"]))
+		} else if statusCode == 404 {
+			err = errors.New(fmt.Sprintf("No manageable entities matching the request criteria found: %v (%v)\n", respProperties["statusCode"], respProperties["statusDescription"]))
+		} else {
+			err = errors.New(fmt.Sprintf("AMQP Management operation wasn't successfull: %v (%v)\n", respProperties["statusCode"], respProperties["statusDescription"]))
+		}
+	} else {
+		err = errors.New(fmt.Sprintf("AMQP Management operation failed: %v\n", err.Error()))
+	}
+
+	return
+}
+
+func parseReadResults(body interface{}) (rows [][]string) {
+	for attribute, value := range map[interface{}]interface{}(body.(amqp.Map)) {
+		row := make([]string, 2)
+		row[0] = attribute.(string)
+		row[1] = fmt.Sprintf("%v", value)
+		rows = append(rows, row)
+	}
+
+	return
+}
